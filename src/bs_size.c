b955894d19afe54944940297a888d4333a27855e
Vratislav Podzimek <vpodzime@redhat.com>
2015-09-30 13:44:44 +0200

    Add a from_string constructor and a method to get bytes as a string
    Being able to create a new BSSize instance from a string and to return the
    number of bytes as a string is the crucial first step allowing the
    implementation to support sizes bigger than what fits into an unsigned 64bit
    integer.
    
    Also fix some minor issues in the basic implementation.

--- src/bs_size.c
+++ src/bs_size.c
@@ -1,8 +1,11 @@
 #include <glib.h>
 #include <glib-object.h>
 #include <gmp.h>
+#include <langinfo.h>
 
 #include "bs_size.h"
+
+#define N_(String) String
 
 /**
  * SECTION: Size
@@ -15,6 +18,18 @@
  * %G_MAXUINT64 and doing calculations with sizes without loss of
  * precision/information.
  */
+
+/* static void* realloc_old_new (void *ptr, gsize old_size __attribute__ ((unused)), gsize new_size) { */
+/*     return g_try_realloc (ptr, new_size); */
+/* } */
+
+/* static void free_old (void *ptr, gsize old_size __attribute__ ((unused))) { */
+/*     return g_free (ptr); */
+/* } */
+
+/* static void init (void) { */
+/*     mp_set_memory_functions (g_try_malloc, realloc_old_new, free_old); */
+/* } */
 
 /**
  * BSSize:
@@ -45,6 +60,17 @@ struct _BSSizePrivate {
     mpz_t bytes;
 };
 
+static gchar const * const b_units[BS_BUNIT_UNDEF] = {N_("B"), N_("KiB"), N_("MiB"), N_("GiB"), N_("TiB"),
+                                                      N_("PiB"), N_("EiB"), N_("ZiB"), N_("YiB")};
+
+static gchar const * const d_units[BS_DUNIT_UNDEF] = {N_("B"), N_("KB"), N_("MB"), N_("GB"), N_("TB"),
+                                                      N_("PB"), N_("EB"), N_("ZB"), N_("YB")};
+
+GQuark bs_size_error_quark (void)
+{
+    return g_quark_from_static_string ("g-bs-size-error-quark");
+}
+
 G_DEFINE_TYPE (BSSize, bs_size, G_TYPE_OBJECT)
 
 static void bs_size_dispose(GObject *size);
@@ -72,16 +98,152 @@ static void bs_size_dispose (GObject *object) {
     G_OBJECT_CLASS(bs_size_parent_class)->dispose (object);
 }
 
-BSSize* bs_size_new () {
+BSSize* bs_size_new (void) {
     return BS_SIZE (g_object_new (BS_TYPE_SIZE, NULL));
 }
 
-BSSize* bs_size_new_from_bytes (guint64 bytes) {
+BSSize* bs_size_new_from_bytes (guint64 bytes, GError **error __attribute__((unused))) {
     BSSize *ret = bs_size_new ();
     mpz_set_ui (ret->priv->bytes, bytes);
     return ret;
 }
 
-guint64 bs_size_get_bytes (BSSize *size) {
+/**
+ * replace_char:
+ *
+ * Replaces all appereances of @orig in @str with @new (in place).
+ */
+static gchar *replace_char (gchar *str, gchar orig, gchar new) {
+    gchar *pos = str;
+    if (!str)
+        return str;
+
+    for (pos=str; pos; pos++)
+        *pos = *pos == orig ? new : *pos;
+
+    return str;
+}
+
+static gboolean multiply_size_by_unit (mpf_t size, gchar *unit_str) {
+    BSBUnit bunit = BS_BUNIT_UNDEF;
+    BSDunit dunit = BS_DUNIT_UNDEF;
+    guint64 pwr = 0;
+    mpf_t dec_mul;
+
+    for (bunit=BS_BUNIT_B; bunit < BS_BUNIT_UNDEF; bunit++)
+        if (g_strcmp0 (unit_str, b_units[bunit]) == 0) {
+            pwr = (guint64) bunit;
+            mpf_mul_2exp (size, size, 10 * pwr);
+            return TRUE;
+        }
+
+    mpf_init2 (dec_mul, BS_FLOAT_PREC_BITS);
+    mpf_set_ui (dec_mul, 1000);
+    for (dunit=BS_BUNIT_B; dunit < BS_DUNIT_UNDEF; dunit++)
+        if (g_strcmp0 (unit_str, d_units[dunit]) == 0) {
+            pwr = (guint64) dunit;
+            mpf_pow_ui (dec_mul, dec_mul, pwr);
+            mpf_mul (size, size, dec_mul);
+            mpf_clear (dec_mul);
+            return TRUE;
+        }
+
+    return FALSE;
+}
+
+BSSize* bs_size_new_from_str (const gchar *size_str, GError **error) {
+    gchar const * const pattern = "(?P<numeric>  # the numeric part consists of three parts, below \n" \
+                                  " (-|\\+)?     # optional sign character \n" \
+                                  " (?P<base>([0-9\\.]+))         # base \n" \
+                                  " (?P<exp>(e|E)(-|\\+)[0-9]+)?) # exponent \n" \
+                                  "\\s*               # white space \n" \
+                                  "(?P<rest>[^\\s]*$) # unit specification";
+    GRegex *regex = NULL;
+    gboolean success = FALSE;
+    GMatchInfo *match_info = NULL;
+    gchar *num_str = NULL;
+    gchar *radix_char = NULL;
+    gchar *loc_size_str = g_strdup (size_str);
+    mpf_t size;
+    gint status = 0;
+    gchar *unit_str = NULL;
+    BSSize *ret = NULL;
+
+    radix_char = nl_langinfo (RADIXCHAR);
+    if (g_strcmp0 (radix_char, ".") != 0)
+        replace_char (loc_size_str, '.', *radix_char);
+
+    regex = g_regex_new (pattern, G_REGEX_EXTENDED, 0, error);
+    if (!regex) {
+        g_free (loc_size_str);
+        /* error is already populated */
+        return NULL;
+    }
+
+    success = g_regex_match (regex, loc_size_str, 0, &match_info);
+    if (!success) {
+        g_set_error (error, BS_SIZE_ERROR, BS_SIZE_ERROR_INVALID_SPEC,
+                     "Failed to parse size spec: %s", size_str);
+        g_regex_unref (regex);
+        g_match_info_free (match_info);
+        g_free (loc_size_str);
+        return NULL;
+    }
+    g_regex_unref (regex);
+
+    num_str = g_match_info_fetch_named (match_info, "numeric");
+    if (!num_str) {
+        g_set_error (error, BS_SIZE_ERROR, BS_SIZE_ERROR_INVALID_SPEC,
+                     "Failed to parse size spec: %s", size_str);
+        g_regex_unref (regex);
+        g_match_info_free (match_info);
+        g_free (loc_size_str);
+        return NULL;
+    }
+
+    mpf_init2 (size, BS_FLOAT_PREC_BITS);
+    status = mpf_set_str (size, num_str, 10);
+    if (status != 0) {
+        g_set_error (error, BS_SIZE_ERROR, BS_SIZE_ERROR_INVALID_SPEC,
+                     "Failed to parse size spec: %s", size_str);
+        g_match_info_free (match_info);
+        g_free (loc_size_str);
+        mpf_clear (size);
+        return NULL;
+    }
+
+    unit_str = g_match_info_fetch_named (match_info, "rest");
+    if (unit_str && g_strcmp0 (unit_str, "") != 0) {
+        g_strstrip (unit_str);
+        if (!multiply_size_by_unit (size, unit_str)) {
+            g_set_error (error, BS_SIZE_ERROR, BS_SIZE_ERROR_INVALID_SPEC,
+                         "Failed to recognize unit from the spec: %s", size_str);
+            g_match_info_free (match_info);
+            g_free (loc_size_str);
+            mpf_clear (size);
+            return NULL;
+        }
+    }
+
+    ret = bs_size_new ();
+    mpz_set_f (ret->priv->bytes, size);
+
+    g_free (loc_size_str);
+    g_match_info_free (match_info);
+    mpf_clear (size);
+
+    return ret;
+}
+
+guint64 bs_size_get_bytes (BSSize *size, GError **error) {
+    if (mpz_cmp_ui (size->priv->bytes, G_MAXUINT64) > 0) {
+        g_set_error (error, BS_SIZE_ERROR, BS_SIZE_ERROR_OVER,
+                     "The size is too big, cannot be returned as a 64bit number of bytes");
+        return 0;
+    }
     return (guint64) mpz_get_ui (size->priv->bytes);
+}
+
+gchar* bs_size_get_bytes_str (BSSize *size, GError **error __attribute__((unused))) {
+    return mpz_get_str (NULL, 10, size->priv->bytes);
 }
--- src/bs_size.h
+++ src/bs_size.h
@@ -13,30 +13,71 @@ G_BEGIN_DECLS
 #define BS_IS_SIZE_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE ((klass), BS_TYPE_SIZE))
 #define BS_SIZE_GET_CLASS(obj)  (G_TYPE_INSTANCE_GET_CLASS ((obj), BS_TYPE_SIZE, BSSizeClass))
 
+#define BS_SIZE_ERROR bs_size_error_quark ()
+typedef enum {
+    BS_SIZE_ERROR_INVALID_SPEC,
+    BS_SIZE_ERROR_OVER,
+} BSSizeError;
+
+typedef enum {
+    BS_BUNIT_B = 0, BS_BUNIT_KiB, BS_BUNIT_MiB, BS_BUNIT_GiB, BS_BUNIT_TiB,
+    BS_BUNIT_PiB, BS_BUNIT_EiB, BS_BUNIT_ZiB, BS_BUNIT_YiB, BS_BUNIT_UNDEF,
+} BSBUnit;
+
+typedef enum {
+    BS_DUNIT_B = 0, BS_DUNIT_KB, BS_DUNIT_MB, BS_DUNIT_GB, BS_DUNIT_TB,
+    BS_DUNIT_PB, BS_DUNIT_EB, BS_DUNIT_ZB, BS_DUNIT_YB, BS_DUNIT_UNDEF,
+} BSDunit;
+
+/* use 256 bits of precision for floating point numbets, that should be more
+   than enough */
+#define BS_FLOAT_PREC_BITS 256
+
 typedef struct _BSSize        BSSize;
 typedef struct _BSSizeClass   BSSizeClass;
 typedef struct _BSSizePrivate BSSizePrivate;
 
-GType bs_size_get_type ();
+GType bs_size_get_type (void);
 
 /**
  * bs_size_new_from_bytes: (constructor)
  *
  * Creates a new #BSSize instance.
  *
- * Returns: (transfer full): a new #BSSize
+ * Returns: a new #BSSize
  */
-BSSize* bs_size_new_from_bytes (guint64 bytes);
+BSSize* bs_size_new_from_bytes (guint64 bytes, GError **error);
+
+/**
+ * bs_size_new_from_str: (constructor)
+ *
+ * Creates a new #BSSize instance.
+ *
+ * Returns: a new #BSSize
+ */
+BSSize* bs_size_new_from_str (const gchar *size_str, GError **error);
 
 /**
  * bs_size_new: (constructor)
  * Creates a new #BSSize instance.
  *
- * Returns: (transfer full): a new #BSSize
+ * Returns: a new #BSSize
  */
-BSSize* bs_size_new ();
+BSSize* bs_size_new (void);
 
-guint64 bs_size_get_bytes (BSSize *size);
+/**
+ * bs_size_get_bytes:
+ *
+ * Returns: the @size in a number of bytes.
+ */
+guint64 bs_size_get_bytes (BSSize *size, GError **error);
+
+/**
+ * bs_size_get_bytes_str:
+ *
+ * Returns: (transfer full): the string representing the @size as a number of bytes.
+ */
+gchar* bs_size_get_bytes_str (BSSize *size, GError **error);
 
 G_END_DECLS
 
