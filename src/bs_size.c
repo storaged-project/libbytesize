73a711e80ed814514c60f68b117c324b442073b0
Vratislav Podzimek <vpodzime@redhat.com>
2015-10-09 11:11:38 +0200

    Improve the documentation of our types and functions
    Including marking things that we could but don't change as 'const'.

--- src/bs_size.c
+++ src/bs_size.c
@@ -10,28 +10,23 @@
 #define N_(String) String
 
 /**
- * SECTION: Size
+ * SECTION: size
  * @title: BSSize
  * @short_description: a class facilitating work with sizes in bytes
+ * @include: bs_size.h
  *
  * A #BSSize is class that facilitates work with sizes in bytes by providing
  * functions/methods that are required for parsing users input when entering
  * size, showing size in nice human-readable format, storing sizes bigger than
  * %G_MAXUINT64 and doing calculations with sizes without loss of
  * precision/information.
+ *
+ * The reason why some functions take or return a float as a string instead of a
+ * float directly is because a string "0.3" can be translated into 0.3 with
+ * appropriate precision while 0.3 as float is probably something like
+ * 0.294343... with unknown precisi
+ *
  */
-
-/* static void* realloc_old_new (void *ptr, gsize old_size __attribute__ ((unused)), gsize new_size) { */
-/*     return g_try_realloc (ptr, new_size); */
-/* } */
-
-/* static void free_old (void *ptr, gsize old_size __attribute__ ((unused))) { */
-/*     return g_free (ptr); */
-/* } */
-
-/* static void init (void) { */
-/*     mp_set_memory_functions (g_try_malloc, realloc_old_new, free_old); */
-/* } */
 
 
 /***************
@@ -114,9 +109,10 @@ static void bs_size_dispose (GObject *object) {
  ****************/
 /**
  * bs_size_new: (constructor)
- * Creates a new #BSSize instance.
  *
- * Returns: a new #BSSize
+ * Creates a new #BSSize instance initialized to 0.
+ *
+ * Returns: a new #BSSize initialized to 0.
  */
 BSSize* bs_size_new (void) {
     return BS_SIZE (g_object_new (BS_TYPE_SIZE, NULL));
@@ -124,12 +120,14 @@ BSSize* bs_size_new (void) {
 
 /**
  * bs_size_new_from_bytes: (constructor)
+ * @bytes: number of bytes
+ * @sgn: sign of the size - if being -1, the size is initialized to -@bytes
  *
  * Creates a new #BSSize instance.
  *
  * Returns: a new #BSSize
  */
-BSSize* bs_size_new_from_bytes (guint64 bytes, gint sgn, GError **error __attribute__((unused))) {
+BSSize* bs_size_new_from_bytes (guint64 bytes, gint sgn) {
     BSSize *ret = bs_size_new ();
     mpz_set_ui (ret->priv->bytes, bytes);
     if (sgn == -1)
@@ -138,7 +136,7 @@ BSSize* bs_size_new_from_bytes (guint64 bytes, gint sgn, GError **error __attrib
 }
 
 /**
- * replace_char:
+ * replace_char: (skip)
  *
  * Replaces all appereances of @orig in @str with @new (in place).
  */
@@ -206,6 +204,8 @@ static gboolean multiply_size_by_unit (mpf_t size, gchar *unit_str) {
 
 /**
  * bs_size_new_from_str: (constructor)
+ * @size_str: string representing the size as a number and an optional unit
+ *            (e.g. "1 GiB")
  *
  * Creates a new #BSSize instance.
  *
@@ -297,10 +297,13 @@ BSSize* bs_size_new_from_str (const gchar *size_str, GError **error) {
 
 /**
  * bs_size_new_from_size: (constructor)
+ * @size: the size to create a new instance from (a copy of)
+ *
+ * Creates a new instance of #BSSize.
  *
  * Returns: (transfer full): a new #BSSize instance which is copy of @size.
  */
-BSSize* bs_size_new_from_size (BSSize *size, GError **error __attribute__((unused))) {
+BSSize* bs_size_new_from_size (const BSSize *size) {
     BSSize *ret = NULL;
 
     ret = bs_size_new ();
@@ -315,45 +318,58 @@ BSSize* bs_size_new_from_size (BSSize *size, GError **error __attribute__((unuse
  *****************/
 /**
  * bs_size_get_bytes:
- * @sgn: (out): sign of the @size
+ * @sgn: (allow-none) (out): sign of the @size - -1, 0 or 1 for negative, zero or positive
+ *                           size respectively
+ *
+ * Get the number of bytes of the @size.
  *
  * Returns: the @size in a number of bytes
  */
-guint64 bs_size_get_bytes (BSSize *size, gint *sgn, GError **error) {
+guint64 bs_size_get_bytes (const BSSize *size, gint *sgn, GError **error) {
     if (mpz_cmp_ui (size->priv->bytes, G_MAXUINT64) > 0) {
         g_set_error (error, BS_SIZE_ERROR, BS_SIZE_ERROR_OVER,
                      "The size is too big, cannot be returned as a 64bit number of bytes");
         return 0;
     }
-    *sgn = mpz_sgn (size->priv->bytes);
+    if (sgn)
+        *sgn = mpz_sgn (size->priv->bytes);
     return (guint64) mpz_get_ui (size->priv->bytes);
 }
 
 /**
  * bs_size_sgn:
  *
+ * Get the sign of the size.
+ *
  * Returns: -1, 0 or 1 if @size is negative, zero or positive, respectively
  */
-gint bs_size_sgn (BSSize *size) {
+gint bs_size_sgn (const BSSize *size) {
     return mpz_sgn (size->priv->bytes);
 }
 
 /**
  * bs_size_get_bytes_str:
  *
+ * Get the number of bytes in @size as a string. This way, the caller doesn't
+ * have to care about the limitations of some particular integer type.
+ *
  * Returns: (transfer full): the string representing the @size as a number of bytes.
  */
-gchar* bs_size_get_bytes_str (BSSize *size, GError **error __attribute__((unused))) {
+gchar* bs_size_get_bytes_str (const BSSize *size) {
     return mpz_get_str (NULL, 10, size->priv->bytes);
 }
 
 /**
  * bs_size_convert_to:
+ * @unit: the unit to convert @size to
+ *
+ * Get the @size converted to @unit as a string representing a floating-point
+ * number.
  *
  * Returns: (transfer full): a string representing the floating-point number
  *                           that equals to @size converted to @unit
  */
-gchar* bs_size_convert_to (BSSize *size, BSUnit unit, GError **error) {
+gchar* bs_size_convert_to (const BSSize *size, BSUnit unit, GError **error) {
     BSBunit b_unit = BS_BUNIT_B;
     BSDunit d_unit = BS_DUNIT_B;
     mpf_t divisor;
@@ -398,12 +414,17 @@ gchar* bs_size_convert_to (BSSize *size, BSUnit unit, GError **error) {
 
 /**
  * bs_size_human_readable:
+ * @min_unit: the smallest unit the returned representation should use
+ * @max_places: maximum number of decimal places the representation should use
+ * @xlate: whether to try to translate the representation or not
+ *
+ * Get a human-readable representation of @size.
  *
  * Returns: (transfer full): a string which is human-readable representation of
  *                           @size according to the restrictions given by the
  *                           other parameters
  */
-gchar* bs_size_human_readable (BSSize *size, BSBunit min_unit, gint max_places, gboolean xlate) {
+gchar* bs_size_human_readable (const BSSize *size, BSBunit min_unit, gint max_places, gboolean xlate) {
     mpf_t cur_val;
     gchar *num_str = NULL;
     gchar *ret = NULL;
@@ -434,6 +455,7 @@ gchar* bs_size_human_readable (BSSize *size, BSBunit min_unit, gint max_places, 
                         cur_val);
     mpf_clear (cur_val);
 
+    /* TODO: should use the proper radix char according to @xlate */
     /* remove trailing zeros and the radix char */
     radix_char = nl_langinfo (RADIXCHAR);
     zero = num_str + (len - 1);
@@ -454,9 +476,11 @@ gchar* bs_size_human_readable (BSSize *size, BSBunit min_unit, gint max_places, 
 /**
  * bs_size_add:
  *
+ * Add two sizes.
+ *
  * Returns: (transfer full): a new instance of #BSSize which is a sum of @size1 and @size2
  */
-BSSize* bs_size_add (BSSize *size1, BSSize *size2) {
+BSSize* bs_size_add (const BSSize *size1, const BSSize *size2) {
     BSSize *ret = bs_size_new ();
     mpz_add (ret->priv->bytes, size1->priv->bytes, size2->priv->bytes);
 
@@ -466,9 +490,12 @@ BSSize* bs_size_add (BSSize *size1, BSSize *size2) {
 /**
  * bs_size_add_bytes:
  *
+ * Add @bytes to the @size. To add a negative number of bytes use
+ * bs_size_sub_bytes().
+ *
  * Returns: (transfer full): a new instance of #BSSize which is a sum of @size and @bytes
  */
-BSSize* bs_size_add_bytes (BSSize *size, guint64 bytes) {
+BSSize* bs_size_add_bytes (const BSSize *size, guint64 bytes) {
     BSSize *ret = bs_size_new ();
     mpz_add_ui (ret->priv->bytes, size->priv->bytes, bytes);
 
@@ -478,9 +505,11 @@ BSSize* bs_size_add_bytes (BSSize *size, guint64 bytes) {
 /**
  * bs_size_sub:
  *
+ * Subtract @size2 from @size1.
+ *
  * Returns: (transfer full): a new instance of #BSSize which is equals to @size1 - @size2
  */
-BSSize* bs_size_sub (BSSize *size1, BSSize *size2) {
+BSSize* bs_size_sub (const BSSize *size1, const BSSize *size2) {
     BSSize *ret = bs_size_new ();
     mpz_sub (ret->priv->bytes, size1->priv->bytes, size2->priv->bytes);
 
@@ -490,9 +519,12 @@ BSSize* bs_size_sub (BSSize *size1, BSSize *size2) {
 /**
  * bs_size_sub_bytes:
  *
+ * Subtract @bytes from the @size. To subtract a negative number of bytes use
+ * bs_size_add_bytes().
+ *
  * Returns: (transfer full): a new instance of #BSSize which is equals to @size - @bytes
  */
-BSSize* bs_size_sub_bytes (BSSize *size, guint64 bytes) {
+BSSize* bs_size_sub_bytes (const BSSize *size, guint64 bytes) {
     BSSize *ret = bs_size_new ();
     mpz_sub_ui (ret->priv->bytes, size->priv->bytes, bytes);
 
@@ -502,9 +534,11 @@ BSSize* bs_size_sub_bytes (BSSize *size, guint64 bytes) {
 /**
  * bs_size_mul_int:
  *
+ * Multiply @size by @times.
+ *
  * Returns: (transfer full): a new instance of #BSSize which is equals to @size * @times
  */
-BSSize* bs_size_mul_int (BSSize *size, guint64 times) {
+BSSize* bs_size_mul_int (const BSSize *size, guint64 times) {
     BSSize *ret = bs_size_new ();
     mpz_mul_ui (ret->priv->bytes, size->priv->bytes, times);
 
@@ -514,15 +548,13 @@ BSSize* bs_size_mul_int (BSSize *size, guint64 times) {
 /**
  * bs_size_mul_float_str:
  *
- * Returns: (transfer full): a new #BSSize instance which equals to @size *
- * @times_str
+ * Multiply @size by the floating-point number @float_str represents.
  *
- * The reason why this function takes a float as a string instead of a float
- * directly is because a string "0.3" can be translated into 0.3 with
- * appropriate precision while 0.3 as float is probably something like
- * 0.294343... with unknown precision.
+ * Returns: (transfer full): a new #BSSize instance which equals to
+ *                           @size * @times_str
+ *
  */
-BSSize* bs_size_mul_float_str (BSSize *size, const gchar *float_str, GError **error) {
+BSSize* bs_size_mul_float_str (const BSSize *size, const gchar *float_str, GError **error) {
     mpf_t op1, op2;
     gint status = 0;
     BSSize *ret = NULL;
@@ -551,10 +583,13 @@ BSSize* bs_size_mul_float_str (BSSize *size, const gchar *float_str, GError **er
 /**
  * bs_size_div:
  *
+ * Divide @size1 by @size2. Gives the answer to the question "How many times
+ * does @size2 fit in @size1?".
+ *
  * Returns: integer number x so that x * @size1 < @size2 and (x+1) * @size1 > @size2
  *          (IOW, @size1 / @size2 using integer division)
  */
-guint64 bs_size_div (BSSize *size1, BSSize *size2, GError **error) {
+guint64 bs_size_div (const BSSize *size1, const BSSize *size2, GError **error) {
     mpz_t result;
     guint64 ret = 0;
 
@@ -582,10 +617,13 @@ guint64 bs_size_div (BSSize *size1, BSSize *size2, GError **error) {
 /**
  * bs_size_div_int:
  *
+ * Divide @size by @divisor. Gives the answer to the question "What is the size
+ * of each chunk if @size is split into a @divisor number of pieces?"
+ *
  * Returns: (transfer full): a #BSSize instance x so that x * @divisor = @size,
  *                           rounded to a number of bytes
  */
-BSSize* bs_size_div_int (BSSize *size, guint64 divisor, GError **error) {
+BSSize* bs_size_div_int (const BSSize *size, guint64 divisor, GError **error) {
     BSSize *ret = NULL;
 
     if (divisor == 0) {
@@ -603,10 +641,12 @@ BSSize* bs_size_div_int (BSSize *size, guint64 divisor, GError **error) {
 /**
  * bs_size_true_div:
  *
+ * Divides @size1 by @size2.
+ *
  * Returns: (transfer full): a string representing the floating-point number
  *                           that equals to @size1 / @size2
  */
-gchar* bs_size_true_div (BSSize *size1, BSSize *size2, GError **error) {
+gchar* bs_size_true_div (const BSSize *size1, const BSSize *size2, GError **error) {
     mpf_t op1;
     mpf_t op2;
     gchar *ret = NULL;
@@ -634,10 +674,12 @@ gchar* bs_size_true_div (BSSize *size1, BSSize *size2, GError **error) {
 /**
  * bs_size_true_div_int:
  *
+ * Divides @size by @divisor.
+ *
  * Returns: (transfer full): a string representing the floating-point number
  *                           that equals to @size / @divisor
  */
-gchar* bs_size_true_div_int (BSSize *size, guint64 divisor, GError **error) {
+gchar* bs_size_true_div_int (const BSSize *size, guint64 divisor, GError **error) {
     mpf_t op1;
     gchar *ret = NULL;
 
@@ -662,10 +704,13 @@ gchar* bs_size_true_div_int (BSSize *size, guint64 divisor, GError **error) {
 /**
  * bs_size_mod:
  *
+ * Gives @size1 modulo @size2 (i.e. the remainder of integer division @size1 /
+ * @size2).
+ *
  * Returns: (transfer full): a #BSSize instance that is a remainder of
  *                           @size1 / @size2 using integer division
  */
-BSSize* bs_size_mod (BSSize *size1, BSSize *size2, GError **error) {
+BSSize* bs_size_mod (const BSSize *size1, const BSSize *size2, GError **error) {
     BSSize *ret = NULL;
     if (mpz_cmp_ui (size2->priv->bytes, 0) == 0) {
         g_set_error (error, BS_SIZE_ERROR, BS_SIZE_ERROR_ZERO_DIV,
@@ -682,11 +727,18 @@ BSSize* bs_size_mod (BSSize *size1, BSSize *size2, GError **error) {
 
 /**
  * bs_size_round_to_nearest:
+ * @round_to: to a multiple of what to round @size
+ * @dir: %BS_ROUND_DIR_UP to round up (to the nearest multiple of @round_to
+ *       bigger than @size) or %BS_ROUND_DIR_DOWN to round down (to the
+ *       nearest multiple of @round_to smaller than @size)
+ *
+ * Round @size to the nearest multiple of @round_to according to the direction
+ * given by @dir.
  *
  * Returns: (transfer full): a new instance of #BSSize that is @size rounded to
  *                           a multiple of @round_to according to @dir
  */
-BSSize* bs_size_round_to_nearest (BSSize *size, BSSize *round_to, BSRoundDir dir, GError **error) {
+BSSize* bs_size_round_to_nearest (const BSSize *size, const BSSize *round_to, BSRoundDir dir, GError **error) {
     BSSize *ret = NULL;
     mpz_t q;
 
@@ -717,11 +769,15 @@ BSSize* bs_size_round_to_nearest (BSSize *size, BSSize *round_to, BSRoundDir dir
  ***************/
 /**
  * bs_size_cmp:
+ * @abs: whether to compare absolute values of @size1 and @size2 instead
+ *
+ * Compare @size1 and @size2. This function behaves like the standard *cmp*()
+ * functions.
  *
  * Returns: -1, 0, or 1 if @size1 is smaller, equal to or bigger than
  *          @size2 respectively comparing absolute values if @abs is %TRUE
  */
-gint bs_size_cmp (BSSize *size1, BSSize *size2, gboolean abs) {
+gint bs_size_cmp (const BSSize *size1, const BSSize *size2, gboolean abs) {
     if (abs)
         return mpz_cmpabs (size1->priv->bytes, size2->priv->bytes);
     else
@@ -730,13 +786,17 @@ gint bs_size_cmp (BSSize *size1, BSSize *size2, gboolean abs) {
 
 /**
  * bs_size_cmp_bytes:
+ * @abs: whether to compare absolute values of @size and @bytes instead.
  *
- * Returns: -1, 0, or 1 if @size1 is smaller, equal to or bigger than
+ * Compare @size and @bytes, i.e. the number of bytes @size has with
+ * @bytes. This function behaves like the standard *cmp*() functions.
+ *
+ * Returns: -1, 0, or 1 if @size is smaller, equal to or bigger than
  *          @bytes respectively comparing absolute values if @abs is %TRUE
  */
-gint bs_size_cmp_bytes (BSSize *size1, guint64 bytes, gboolean abs) {
+gint bs_size_cmp_bytes (const BSSize *size, guint64 bytes, gboolean abs) {
     if (abs)
-        return mpz_cmpabs_ui (size1->priv->bytes, bytes);
+        return mpz_cmpabs_ui (size->priv->bytes, bytes);
     else
-        return mpz_cmp_ui (size1->priv->bytes, bytes);
+        return mpz_cmp_ui (size->priv->bytes, bytes);
 }
