3e22b14a1fd3923158bcc61b4e3bb6c3f9e9a859
Vojtech Trefny <vtrefny@redhat.com>
2023-03-01 11:06:18 +0100

    Make use of error optional

--- src/bs_size.c
+++ src/bs_size.c
@@ -280,6 +280,11 @@ static bool multiply_size_by_unit (mpfr_t size, char *unit_str) {
  * Sets @error to @code and @msg (if not %NULL). **TAKES OVER @msg.**
  */
 static void set_error (BSError **error, BSErrorCode code, char *msg) {
+    if (error == NULL) {
+        free (msg);
+        return;
+    }
+
     *error = (BSError *) malloc (sizeof(BSError));
     if (*error == NULL) {
         free (msg);
@@ -421,6 +426,7 @@ BSSize bs_size_new_from_bytes (uint64_t bytes, int sgn) {
  * bs_size_new_from_str: (constructor)
  * @size_str: string representing the size as a number and an optional unit
  *            (e.g. "1 GiB")
+ * @error: (out) (optional): place to store error (if any)
  *
  * Creates a new #BSSize instance.
  *
@@ -579,6 +585,7 @@ BSSize bs_size_new_from_size (const BSSize size) {
  * bs_size_get_bytes:
  * @sgn: (allow-none) (out): sign of the @size - -1, 0 or 1 for negative, zero or positive
  *                           size respectively
+ * @error: (out) (optional): place to store error (if any)
  *
  * Get the number of bytes of the @size.
  *
@@ -643,6 +650,7 @@ char* bs_size_get_bytes_str (const BSSize size) {
 /**
  * bs_size_convert_to:
  * @unit: the unit to convert @size to
+ * @error: (out) (optional): place to store error (if any)
  *
  * Get the @size converted to @unit as a string representing a floating-point
  * number.
@@ -920,6 +928,7 @@ BSSize bs_size_grow_mul_int (BSSize size, uint64_t times) {
 
 /**
  * bs_size_mul_float_str:
+ * @error: (out) (optional): place to store error (if any)
  *
  * Multiply @size by the floating-point number @float_str represents.
  *
@@ -961,6 +970,7 @@ BSSize bs_size_mul_float_str (const BSSize size, const char *float_str, BSError 
 
 /**
  * bs_size_grow_mul_float_str:
+ * @error: (out) (optional): place to store error (if any)
  *
  * Grow @size by the floating-point number @float_str represents times. IOW,
  * multiply @size by @float_str in-place.
@@ -1002,6 +1012,7 @@ BSSize bs_size_grow_mul_float_str (BSSize size, const char *float_str, BSError *
 /**
  * bs_size_div:
  * @sgn: (allow-none) (out): sign of the result
+ * @error: (out) (optional): place to store error (if any)
  *
  * Divide @size1 by @size2. Gives the answer to the question "How many times
  * does @size2 fit in @size1?".
@@ -1036,6 +1047,7 @@ uint64_t bs_size_div (const BSSize size1, const BSSize size2, int *sgn, BSError 
 
 /**
  * bs_size_div_int:
+ * @error: (out) (optional): place to store error (if any)
  *
  * Divide @size by @divisor. Gives the answer to the question "What is the size
  * of each chunk if @size is split into a @divisor number of pieces?"
@@ -1065,6 +1077,7 @@ BSSize bs_size_div_int (const BSSize size, uint64_t divisor, BSError **error) {
 
 /**
  * bs_size_shrink_div_int:
+ * @error: (out) (optional): place to store error (if any)
  *
  * Shrink @size by dividing by @divisor. IOW, divide @size by @divisor in-place.
  *
@@ -1092,6 +1105,7 @@ BSSize bs_size_shrink_div_int (BSSize size, uint64_t divisor, BSError **error) {
 
 /**
  * bs_size_true_div:
+ * @error: (out) (optional): place to store error (if any)
  *
  * Divides @size1 by @size2.
  *
@@ -1124,6 +1138,7 @@ char* bs_size_true_div (const BSSize size1, const BSSize size2, BSError **error)
 
 /**
  * bs_size_true_div_int:
+ * @error: (out) (optional): place to store error (if any)
  *
  * Divides @size by @divisor.
  *
@@ -1160,6 +1175,7 @@ char* bs_size_true_div_int (const BSSize size, uint64_t divisor, BSError **error
 
 /**
  * bs_size_mod:
+ * @error: (out) (optional): place to store error (if any)
  *
  * Gives @size1 modulo @size2 (i.e. the remainder of integer division @size1 /
  * @size2). Gives the answer to the question "If I split @size1 into chunks of
@@ -1196,6 +1212,7 @@ BSSize bs_size_mod (const BSSize size1, const BSSize size2, BSError **error) {
  * @dir: %BS_ROUND_DIR_UP to round up (to the nearest multiple of @round_to
  *       bigger than @size) or %BS_ROUND_DIR_DOWN to round down (to the
  *       nearest multiple of @round_to smaller than @size)
+ * @error: (out) (optional): place to store error (if any)
  *
  * Round @size to the nearest multiple of @round_to according to the direction
  * given by @dir.
